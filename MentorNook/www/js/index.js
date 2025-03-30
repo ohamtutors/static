e2cbe0d9f47906600c6a29ff10e8f7c30a9f3b93
ohamtutors <115445793+ohamtutors@users.noreply.github.com>
2025-03-30 09:20:22 +0530

    Update index.js

--- MentorNook/www/js/index.js
+++ MentorNook/www/js/index.js
@@ -21,18 +21,6 @@
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    console.log('Cordova is initialized.');
-
-    // Ensure the splash screen hides after 3 seconds
-    setTimeout(function () {
-        if (navigator.splashscreen) {
-            navigator.splashscreen.hide();
-            console.log('Splash screen hidden');
-        } else {
-            console.log('Splash screen plugin not found');
-        }
-    }, 1000);
 
     // Override the back button behavior
     document.addEventListener('backbutton', function (e) {
